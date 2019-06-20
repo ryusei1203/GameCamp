d74319e930d608b65a6cb430834c0ea4b34d32bd
ryusei1203 <ryusei.jp1203@gmail.com>
2019-06-20 13:31:27 +0900

    Sourceファイルにコードを詰め込んだ

--- ゲーム合宿作品(タイトル未定)/ゲーム合宿作品(タイトル未定)/Source/ObjectBase/ObjectBase.h
+++ ゲーム合宿作品(タイトル未定)/ゲーム合宿作品(タイトル未定)/Source/ObjectBase/ObjectBase.h
@@ -7,7 +7,8 @@ class ObjectBase {
 public:
 	// コンストラクタ
 	ObjectBase() :
-		m_pos({ 0.f,0.f })
+		m_pos({ 0.f,0.f }),
+		speed(0.f)
 	{};
 	// 更新
 	virtual void Update() = 0;
@@ -18,6 +19,8 @@ public:
 protected:
 	// 座標
 	D3DXVECTOR2 m_pos;
+	// 速さ
+	float speed;
 	//// 基本情報
 	//struct Info {
 	//	// 座標
