a9e73b4908ac476d3a3621650d7b40eca2a6eede
ryusei1203 <ryusei.jp1203@gmail.com>
2019-10-08 13:59:58 +0900

    気になったところの手直し、進捗メモ追加

--- ゲーム合宿作品(タイトル未定)/ゲーム合宿作品(タイトル未定)/Source/Lib/Collision/Collision.cpp
+++ ゲーム合宿作品(タイトル未定)/ゲーム合宿作品(タイトル未定)/Source/Lib/Collision/Collision.cpp
@@ -4,7 +4,7 @@
 
 /*----上壁、左壁の当たり判定----*/
 bool Collision::IsHitTopOrLeftWall(float pos) {
-	if (pos <= 0) {
+	if (pos < 0) {
 		return true;
 	}
 	return false;
@@ -13,7 +13,7 @@ bool Collision::IsHitTopOrLeftWall(float pos) {
 
 /*----下壁の当たり判定----*/
 bool Collision::IsHitBottomWall(float pos) {
-	if (pos >= Window::WINDOW_H) {
+	if (pos > Window::WINDOW_H) {
 		return true;
 	}
 	return false;
@@ -22,7 +22,7 @@ bool Collision::IsHitBottomWall(float pos) {
 
 /*----右壁の当たり判定----*/
 bool Collision::IsHitRightWall(float pos) {
-	if (pos >= Window::WINDOW_W) {
+	if (pos > Window::WINDOW_W) {
 		return true;
 	}
 	return false;
@@ -33,10 +33,10 @@ bool Collision::IsHitRightWall(float pos) {
 bool Collision::IsHitSquares(
 	D3DXVECTOR2 top_left_pos1, D3DXVECTOR2 bottom_right_pos1,
 	D3DXVECTOR2 top_left_pos2, D3DXVECTOR2 bottom_right_pos2) {
-	if (top_left_pos1.x <= bottom_right_pos2.x
-		&& top_left_pos2.x <= bottom_right_pos1.x
-		&& top_left_pos1.y <= bottom_right_pos2.y
-		&& top_left_pos2.y <= bottom_right_pos1.y) {
+	if (top_left_pos1.x < bottom_right_pos2.x
+		&& top_left_pos2.x < bottom_right_pos1.x
+		&& top_left_pos1.y < bottom_right_pos2.y
+		&& top_left_pos2.y < bottom_right_pos1.y) {
 		return true;
 	}
 	return false;
